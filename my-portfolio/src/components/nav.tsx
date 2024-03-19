cd35c6c55c99b12ff4f27fcb05a73b7d41312059
jorge1180 <80490272+jorge1180@users.noreply.github.com>
2024-03-18 22:13:02 -0400

    portfolioV2

---
+++ my-portfolio/src/components/nav.tsx
@@ -0,0 +1,20 @@
+import {useState} from 'react'
+
+
+const Nav = () => {
+    const[nav, setNav] = useState(true)
+
+    const handleNav = () => setNav(!nav)
+
+    return (
+        <div className='flex h-16 w-full mx-auto px-10 text-black border-b-2'>
+            <ul className='space-x-4 pt-2  md:flex w-full flex justify-center sm:justify-start'>
+                <li><button className="px-4 py-1 h-10 rounded-md border border-black bg-white text-neutarl-700 text-sm hover:shadow-[4px_4px_0px_0px_rgba(0,0,0)] transition duration-200">Skills</button></li>
+                <li><button className="px-4 py-1 h-10 rounded-md border border-black bg-white text-neutarl-700 text-sm  hover:shadow-[4px_4px_0px_0px_rgba(0,0,0)] transition duration-200">Projects</button></li>
+                <li><button className="px-4 py-1 h-10 rounded-md border border-black bg-white text-neutarl-700 text-sm  hover:shadow-[4px_4px_0px_0px_rgba(0,0,0)] transition duration-200">Contact</button></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Nav
