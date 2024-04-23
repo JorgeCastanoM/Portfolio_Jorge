208c7203649e1e65969b2fe051cf6f6ddec1ffb8
jcastanomejia <jcastanomejia1@ncstudents.niagaracollege.ca>
2024-04-22 23:57:25 -0400

    hero optimize

--- my-portfolio/src/components/background-boxes.tsx
+++ my-portfolio/src/components/background-boxes.tsx
@@ -5,7 +5,7 @@ import { cn } from "../utils/cn";
 const BoxesCore = ({ className, ...rest }: { className?: string }) => {
   const rows = new Array(150).fill(1);
   const cols = new Array(100).fill(1);
-  let colors = [
+  const colors = [
     "--sky-300",
     "--pink-300",
     "--green-300",
@@ -16,39 +16,33 @@ const BoxesCore = ({ className, ...rest }: { className?: string }) => {
     "--indigo-300",
     "--violet-300",
   ];
-  const getRandomColor = () => {
-    return colors[Math.floor(Math.random() * colors.length)];
-  };
+
+  const getRandomColor = () => colors[Math.floor(Math.random() * colors.length)];
 
   return (
     <div
-      style={{
-        transform: `translate(-40%,-60%) skewX(-48deg) skewY(14deg) scale(0.675) rotate(0deg) translateZ(0)`,
-      }}
       className={cn(
-        "absolute left-1/4 p-4 -top-1/4 flex  -translate-x-1/2 -translate-y-1/2 w-full h-full z-0 ",
+        "absolute left-1/4 p-4 -top-1/4 flex -translate-x-1/2 -translate-y-1/2 w-full h-full z-0",
         className
       )}
       {...rest}
     >
       {rows.map((_, i) => (
         <motion.div
-          key={`row` + i}
-          className="w-16 h-8  border-l  border-slate-700 relative"
+          key={`row${i}`}
+          className="w-16 h-8 border-l border-slate-700 relative"
         >
           {cols.map((_, j) => (
             <motion.div
+              key={`col${j}`}
               whileHover={{
                 backgroundColor: `var(${getRandomColor()})`,
                 transition: { duration: 0 },
               }}
-              animate={{
-                transition: { duration: 2 },
-              }}
-              key={`col` + j}
-              className="w-16 h-8  border-r border-t border-slate-700 relative"
+              animate={{ transition: { duration: 2 } }}
+              className="w-16 h-8 border-r border-t border-slate-700 relative"
             >
-              {j % 2 === 0 && i % 2 === 0 ? (
+              {j % 2 === 0 && i % 2 === 0 && (
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -57,13 +51,9 @@ const BoxesCore = ({ className, ...rest }: { className?: string }) => {
                   stroke="currentColor"
                   className="absolute h-6 w-10 -top-[14px] -left-[22px] text-slate-700 stroke-[1px] pointer-events-none"
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 6v12m6-6H6"
-                  />
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m6-6H6" />
                 </svg>
-              ) : null}
+              )}
             </motion.div>
           ))}
         </motion.div>
@@ -74,4 +64,4 @@ const BoxesCore = ({ className, ...rest }: { className?: string }) => {
 
 export const Boxes = React.memo(BoxesCore);
 
-export default BoxesCore
+export default BoxesCore;
